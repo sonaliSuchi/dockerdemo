05b3b3da578e839149eb0ea09333a4411a39a60c
sonaliSuchi <Soni@2347>
2021-04-28 08:26:44 +0200

    test e

--- docker-demo/src/main/java/com/miishhift/dockerdemo/controller/SayHello.java
+++ docker-demo/src/main/java/com/miishhift/dockerdemo/controller/SayHello.java
@@ -1,14 +1,22 @@
 package com.miishhift.dockerdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SayHello {
 
+	@Autowired
+	Environment env;
+	
 	@GetMapping("/hello")
 	public String sayHello()
 	{
-		return "Hi!! Your openshift Deployment is successfull";
+	
+		String hello="test";
+		hello=env.getProperty("hello.test");
+		return "Hi!! Your openshift Deployment is successfull"+hello;
 	}
 }
